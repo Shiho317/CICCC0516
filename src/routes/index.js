c7b5c173b25557f4e98d31a72901243c23f3c879
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-05-16 14:53:30 -0700

    DONE 🫡

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,30 +1,58 @@
-const uuid = require('uuid')
-const router = require('express').Router()
-const {client} = require('../services/redis')
+const uuid = require("uuid");
+const router = require("express").Router();
+const { client } = require("../services/redis");
 
-router.get('/', async (req, res) => {
-    try{
-        const todos = []
-        // const todos = await client.hGetAll('todos')
-        // console.log(todos);
+router.use((req, res, next) => {
+  if (req.query._method === "DELETE") {
+    console.log("DELETE");
+    req.method = "DELETE";
+    req.url = req.path;
+  }
 
-        for await (const { field, value } of client.hScanIterator("todos") ){
-            todos.push({ ID: field, Title: value })
-        }
+  next();
+});
 
-        res.render('index', { model: todos })
-    }catch(e){
-        console.error(e)
+router.get("/", async (req, res) => {
+  try {
+    const todos = [];
+    // const todos = await client.hGetAll('todos')
+    // console.log(todos);
+
+    for await (const { field, value } of client.hScanIterator("todos")) {
+      todos.push({ ID: field, Title: value });
     }
-})
 
-router.get('/create', (req, res) => {
-    res.render('create', { model: {} })
-})
+    res.render("index", { model: todos });
+  } catch (e) {
+    console.error(e);
+  }
+});
 
-router.post('/create', async (req, res) => {
-    await client.hSet("todos", uuid.v4(), req.body.Title)
-    res.redirect('/')
-})
+router.get("/create", (req, res) => {
+  res.render("create", { model: {} });
+});
 
-module.exports = router
+router.post("/create", async (req, res) => {
+  await client.hSet("todos", uuid.v4(), req.body.Title);
+  res.redirect("/");
+});
+
+router.get("/edit/:id", async (req, res) => {
+    const id = req.params.id;
+    const editTodoTitle = await client.hGet("todos", id);
+    res.render("edit", { model: { Title: editTodoTitle, ID: id }})
+});
+
+router.post("/edit/:id", async (req, res) => {
+  const id = req.params.id;
+  await client.hSet("todos", id, req.body.Title);
+  res.redirect("/");
+});
+
+router.delete("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  await client.hDel("todos", id);
+  res.redirect("/");
+});
+
+module.exports = router;
